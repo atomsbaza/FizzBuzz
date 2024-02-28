47b61d1a09b6fbb349ab1a7e4ebe7ebdc8588dec
PISIT KOOLPLUKPOL <pisit.koolplukpol@outlook.com>
2024-02-28 12:50:14 +0700

    Initial commit

---
+++ FizzBuzz.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace balloonone.fizzbuzz
+{
+    public class FizzBuzz
+    {
+        public string ReturnFizzBuzzOutput(int startPoint, int endPoint)
+        {
+            string result = "";
+            ValidateInputs(startPoint, endPoint);
+
+            for (int i = startPoint; i <= endPoint; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    if (i == endPoint)
+                    {
+                        result += $"{i}: FizzBuzz";
+                    }
+                    else
+                    {
+                        result += $"{i}: FizzBuzz\r\n";
+                    }
+                }
+                else if (i % 3 == 0)
+                {
+                    if (i == endPoint)
+                    {
+                        result += $"{i}: Fizz";
+                    }
+                    else
+                    {
+                        result += $"{i}: Fizz\r\n";
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    if (i == endPoint)
+                    {
+                        result += $"{i}: Buzz";
+                    }
+                    else
+                    {
+                        result += $"{i}: Buzz\r\n";
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void ValidateInputs(int startPoint, int endPoint)
+        {
+            // Check start point less than 0 or not
+            if (startPoint < 0)
+            {
+                throw new ArgumentOutOfRangeException("Start Point must equal to or more than 0");
+            }
+
+            // Check end point less than 0 or not
+            if (endPoint < 0)
+            {
+                throw new ArgumentOutOfRangeException("End Point must equal to or more than 0");
+            }
+
+            // Check is start point more than end point or not
+            if (startPoint > endPoint)
+            {
+                throw new ArgumentOutOfRangeException("Start point must less than End point");
+            }
+        }
+
+        public int CheckIsNumber()
+        {
+            // To check that input string are number or not
+            int outPutNumber;
+            bool isValidInput;
+
+            isValidInput = int.TryParse(Console.ReadLine(), out outPutNumber);
+            if (!isValidInput)
+            {
+                throw new ArgumentException("Input value should be number only!!");
+            }
+
+            return outPutNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace balloonone.fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                Console.WriteLine("Enter a non-negative number to start looping from");
+                int startPoint = fizzBuzz.CheckIsNumber();
+                Console.WriteLine("Enter a non - negative number to End looping from");
+                int endPoint = fizzBuzz.CheckIsNumber();
+                string result = fizzBuzz.ReturnFizzBuzzOutput(startPoint, endPoint);
+                Console.WriteLine(result);
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
